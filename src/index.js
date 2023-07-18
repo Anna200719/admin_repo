8f08b31d529d5597f1a0ff2769f246639f0d71d2
Anna200719 <73298456+Anna200719@users.noreply.github.com>
2023-07-18 12:24:37 +0100

    Added final working solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,80 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
-const request = require("request")
+const axios = require("axios")
 const {createObjectCsvWriter} = require("csv-writer")
 
 const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
 
-app.get("/investments/:id", async (req, res) => {
-  const {id} = req.params
-  const userHoldings = null
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
+// Generate CSV report for a specific user
+// eslint-disable-next-line max-statements
+app.get("/generate/:userId", async (req, res) => {
+  const {userId} = req.params
+
+  try {
+    const investmentsResponse = await axios.get(`${config.get("investmentsServiceUrl")}/investments`)
+    const investments = investmentsResponse.data
+
+    const companiesResponse = await axios.get(`${config.get("financialCompaniesServiceUrl")}/companies`)
+    const companies = companiesResponse.data
+
+    const userHoldings = {}
+
+    for (const investment of investments) {
+      if (investment.userId === userId) {
+        for (const holding of investment.holdings) {
+          const company = companies.find((c) => c.id === holding.id)
+          const holdingValue = investment.investmentTotal * holding.investmentPercentage
+
+          if (userHoldings[company.name]) {
+            userHoldings[company.name] += holdingValue
+          } else {
+            userHoldings[company.name] = holdingValue
+          }
+        }
+      }
     }
-  })
+
+    const csvData = Object.entries(userHoldings).map(([holding, value]) => ({
+      User: userId,
+      "First Name": investments[0].firstName,
+      "Last Name": investments[0].lastName,
+      Date: investments[0].date,
+      Holding: holding,
+      Value: value.toFixed(2),
+    }))
+
+    const csvWriter = createObjectCsvWriter({
+      path: "report.csv",
+      header: [
+        {id: "User", title: "User"},
+        {id: "First Name", title: "First Name"},
+        {id: "Last Name", title: "Last Name"},
+        {id: "Date", title: "Date"},
+        {id: "Holding", title: "Holding"},
+        {id: "Value", title: "Value"},
+      ],
+    })
+
+    await csvWriter.writeRecords(csvData)
+
+    await axios.post(`${config.get("investmentsServiceUrl")}/investments/export`, {csvData}, {
+      headers: {"Content-Type": "application/json"},
+    })
+
+    res.send("CSV report generated and sent successfully.")
+  } catch (error) {
+    console.error("Error occurred while sending the CSV report:", error)
+    res.status(500).send("Failed to send the CSV report.")
+  }
 })
 
-const csvWriter = createObjectCsvWriter({
-  path: "report.csv",
-  header: [
-    {id: "User", title: "User"},
-    {id: "First Name", title: "First Name"},
-    {id: "Last Name", title: "Last Name"},
-    {id: "Date", title: "Date"},
-    {id: "Holding", title: "Holding"},
-    {id: "Value", title: "Value"},
-  ],
-})
-
-await csvWriter.writeRecords(userHoldings);
-
-app.listen(config.port, (err) => {
+app.listen(config.get("port"), (err) => {
   if (err) {
     console.error("Error occurred starting the server", err)
     process.exit(1)
   }
-  console.log(`Server running on port ${config.port}`)
+  console.log(`Server running on port ${config.get("port")}`)
 })
