2993bec49d13fe2bb604ec51ff3d0a3fd09115d3
Anna200719 <73298456+Anna200719@users.noreply.github.com>
2023-07-18 11:04:43 +0100

    Added csv structure

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
+const {createObjectCsvWriter} = require("csv-writer")
 
 const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
 
-app.get("/investments/:id", (req, res) => {
+app.get("/investments/:id", async (req, res) => {
   const {id} = req.params
+  const userHoldings = null
   request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
     if (e) {
       console.error(e)
@@ -18,6 +20,20 @@ app.get("/investments/:id", (req, res) => {
     }
   })
 })
+
+const csvWriter = createObjectCsvWriter({
+  path: "report.csv",
+  header: [
+    {id: "User", title: "User"},
+    {id: "First Name", title: "First Name"},
+    {id: "Last Name", title: "Last Name"},
+    {id: "Date", title: "Date"},
+    {id: "Holding", title: "Holding"},
+    {id: "Value", title: "Value"},
+  ],
+})
+
+await csvWriter.writeRecords(userHoldings);
 
 app.listen(config.port, (err) => {
   if (err) {
